f6d00b96858d12fb5ce5f2b64c7252899a4ba76e
Pejman Tabassomi <37804130+ptabasso2@users.noreply.github.com>
2022-12-17 16:06:53 +0100

    Update BasicController.java

--- src/main/java/com/datadog/pej/front/BasicController.java
+++ src/main/java/com/datadog/pej/front/BasicController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.datadog.pej.front.*;
+
 //@RequestMapping("/upstream")
 @RestController
 public class BasicController {
@@ -21,12 +23,17 @@ public class BasicController {
     @Value("#{environment['url'] ?: 'http://localhost:8088'}")
     private String url;
 
+    public long count;
+
     //@RequestMapping("/second/{id}/third")
     @RequestMapping("*/*/*/*")
     public String upstream() {
         int randomValue = 5 + (int)(Math.random() * ((10 - 5) + 1));
+        count = randomValue;
         processingData(randomValue);
-        Quote quote = restTemplate.getForObject(url + "/downstream", Quote.class);
+
+        //Quote quote = restTemplate.getForObject(url + "/downstream", Quote.class);
+        Quote quote = createQuote();
         processingQuote(quote);
         postProcessingQuote(quote);
         LOGGER.info("PathVariable value: ");
@@ -34,18 +41,30 @@ public class BasicController {
     }
 
     public String processingQuote(Quote quote){
+        count = quote.getValue().getId();
         return quote.getType();
     }
 
     public Quote postProcessingQuote(Quote quote){
+        count = quote.getValue().getId() + 12;
         return quote;
     }
 
     public String processingData(int val){
         String localValue;
         localValue = "Alpha" + val;
+        count = val+18;
         return localValue;
     }
 
+    public Quote createQuote(){
+        com.datadog.pej.front.Value value = new com.datadog.pej.front.Value();
+        value.setQuote("How it works!");
+        value.setId(10 + (long)(Math.random() * ((20 - 10) + 1)));
+        Quote quote = new Quote();
+        quote.setType("Success");
+        quote.setValue(value);
+        return quote;
+    }
 
 }
